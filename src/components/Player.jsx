5f4b74d843f43020721f532430fc1bad3a412abf
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-09-16 15:12:53 +0800

    added a state to diplay the winner name on popup & added rematch function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function Player({initialName, symbol, isActive}){
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
-    const [isEditing, setIsEditing] = useState(false);
-    const [playerName, setPlayerName] = useState(initialName);
+  function editHandler() {
+    setIsEditing((editing) => !editing);
 
-    function editHandler(){
-        setIsEditing((editing)=>!editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
     }
+  }
 
-    function handleChange(event){
-        setPlayerName(event.target.value)
-    }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    return (
-        <li className={isActive ? 'active' : undefined}>
-            <span className="player">
-                {!isEditing ? <span className="player-name">{playerName}</span> : 
-                    <input type='text' required value={playerName} onChange={handleChange}></input>}
-            <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={editHandler}>{isEditing ? 'Save': 'Edit'}</button>
-        </li>
-    )
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editHandler}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
